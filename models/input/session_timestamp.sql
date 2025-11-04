a9f359b71d1e145b18fe9136573138a69a50abfe
pratiksha kaushik <Pratiksha.kaushik@sjsu.edu>
2025-11-04 15:29:08 -0800

    Add files via upload

---
+++ models/input/session_timestamp.sql
@@ -0,0 +1,5 @@
+with session_timestamp_cte as (SELECT sessionId,ts
+FROM {{ source('raw', 'session_timestamp') }}
+WHERE sessionId IS NOT NULL
+)
+select * from session_timestamp_cte
---
+++ models/input/user_session_channel.sql
@@ -0,0 +1,9 @@
+with user_session_channel_cte as (
+SELECT userId,sessionId,channel
+FROM {{ source('raw', 'user_session_channel') }}
+WHERE sessionId IS NOT NULL
+)
+select * from user_session_channel_cte
+
+
+
